79976c3c56ca09c8e4c0468b9c04194a688267b4
Anamgulzar67 <69452477+Anamgulzar67@users.noreply.github.com>
2023-12-16 17:33:25 +0500

    final

--- components/Socials.js
+++ components/Socials.js
@@ -17,10 +17,6 @@ const Socials = () => {
       href={'https://www.linkedin.com/in/anam-gulzar-techie/'}
       className="hover:text-accent transition-all duration-300">
         <RiLinkedinLine/>
-      </Link><Link 
-      href={'mailto:anamgulzar67@gmail.com'}
-      className="hover:text-accent transition-all duration-300">
-        <RiMailAddLine/>
       </Link>
       <Link 
       href={'https://www.facebook.com/'}
