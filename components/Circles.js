23ecb14edb7a80f822ec13e4b002b85ea0a1fbcd
Anamgulzar67 <69452477+Anamgulzar67@users.noreply.github.com>
2023-12-22 12:39:26 +0500

    final

--- components/Circles.js
+++ components/Circles.js
@@ -1,18 +1,18 @@
-// next images
-import Image from "next/image"; 
+// // next images
+// import Image from "next/image"; 
 
-const Circles = () => {
-  return(
-   <div className="w-[300px] xl:w-[300px] absolute -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10">
-    <Image 
-    src={'/circles.png'} 
-    width={260} 
-    height={200} 
-    className="w-2/3 xl:w-full xl:h-full"
-    alt=" "
-    />
-   </div>
-  )
-};
+// const Circles = () => {
+//   return(
+//    <div className="w-[300px] xl:w-[300px] absolute -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10">
+//     <Image 
+//     src={'/circles.png'} 
+//     width={260} 
+//     height={200} 
+//     className="w-2/3 xl:w-full xl:h-full"
+//     alt=" "
+//     />
+//    </div>
+//   )
+// };
 
-export default Circles;
+// export default Circles;
